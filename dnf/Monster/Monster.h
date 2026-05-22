f2928b856336dcc048ef4beaba64283e7e75e67c
hocloud7 <hocloud7@gmail.com>
2026-05-22 12:02:40 +0900

    Add: Monster..h /  cpp 신규 추가

--- dnf/Monster/Monster.h
+++ dnf/Monster/Monster.h
@@ -1,7 +1,43 @@
 ﻿#pragma once
 
-class Monster
+#include "GameManager/Actor.h"
+
+class Player;
+
+class Monster : public Actor
 {
 public:
-    
+	Monster(string Name, int Health, int Power);
+
+public:
+	//getter,Setter 생성
+	string GetName() { return Name; }
+	void SetName(string Name) { this->Name = Name; }
+
+	int GetHealth() { return Health; }
+	void SetHealth(int Health) { this->Health = Health; }
+
+	int GetPower() { return Power; }
+	void SetPower(int Power) { this->Power = Power; }
+
+
+public:
+	static Monster SpawnRandomMonster(int PlayerLevel);
+
+public:
+	static Monster SpawnBossMonster(int PlayerLevel);
+
+public:
+	static int GetRandomValue(int Min, int Max);
+
+public:
+	void Attack(Player& Player);
+
+public:
+	void TakeDamage(Player& Player);
+
+protected:
+	string Name;
+	int Health;
+	int Power;
 };
