cdda56f0f4a9a1284a572041f89ec84ffd120def
hocloud7 <hocloud7@gmail.com>
2026-05-22 19:31:51 +0900

    feat: 상점 UI 추가, 기본 변수들 생성

--- dnf/GameSystem/Shop.h
+++ dnf/GameSystem/Shop.h
@@ -1,7 +1,35 @@
 ﻿#pragma once
+#include "../Interface/Actor.h"
+
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Inventory;
+
+struct Product
+{
+    string Name;
+    int Price;
+};
 
 class Shop
 {
+private:
+    vector<Product> Products;
+
 public:
-    
+    Shop();
+
+public:
+    void OpenShop(Inventory& Inventory);
+
+    void ShowProduct();
+
+    void Buy(int ProductIndex, Inventory& Inventory);
+    void Sell();
+
+
+
 };
