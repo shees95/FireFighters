60dbdfa170f213a7722e58c3e1010749673a859c
Choon <gongun124@gmail.com>
2026-05-22 15:41:21 +0900

    feat:Character-TakeDamage, Heal, GainExp, Status

--- dnf/Character/Character.cpp
+++ dnf/Character/Character.cpp
@@ -1,1 +1,75 @@
 ﻿#include "Character.h"
+#include <iostream>
+
+using namespace std;
+
+//싱글톤 패턴을 위한 멤버 변수 초기화
+Character* Character::Instance = nullptr;
+
+//생성자
+Character::Character()
+{
+	Name = Name;
+	Level = 1;
+	Health = 200;
+	MaxHealth = 200;
+	Power = 30;
+	Exp = 0;
+	Gold = 0;
+}
+
+//싱글톤 객체 반환
+Character* Character::GetInstance()
+{
+	//인스턴스가 아직 생성되지 않았다면 새로 생성
+	if (Instance == nullptr)
+	{
+		Instance = new Character();
+	}
+	//생성된 인스턴스 반환
+	return Instance;
+}
+
+//Damage를 입는 함수
+void Character::TakeDamage(int damage)
+{
+	//체력이 damage만큼 감소 (음수방지)
+	SetHealth(max(GetHealth() - damage, 0));
+
+	cout << GetName() << " 이(가) " << damage << " 데미지를 입었습니다!" << endl;
+}
+
+void Character::Heal(int amount)
+{
+	//체력이 amount만큼 회복 (최대체력 초과 방지)
+	SetHealth(min(GetHealth() + amount, GetMaxHealth()));
+
+	cout << GetName() << " 이(가) " << amount << " 체력을 회복했습니다!" << endl;
+}
+
+void Character::GainExp(int amount)
+{
+	// 경험치 증가
+	Exp += amount;
+
+	cout << amount << " 만큼 경험치를 획득하였습니다!" << endl;
+
+	// 경험치가 100 이상인 동안 반복
+	while (Exp >= 100)
+	{
+		// 레벨업에 사용한 경험치 차감
+		Exp -= 100;
+		LevelUp();
+	}
+}
+
+//PlayerStatus 출력
+void Character::PlayerStatus()
+{
+	cout << "Name: " << Name << endl;
+	cout << "Level: " << Level << endl;
+	cout << "Exp: " << Exp << endl;
+	cout << "HP: " << Health << endl;
+	cout << "Power: " << Power << endl;
+	cout << "Gold: " << Gold << endl;
+}
--- dnf/Character/Character.h
+++ dnf/Character/Character.h
@@ -1,7 +1,54 @@
 ﻿#pragma once
+#include <string>
+#include "../Interface/Actor.h"
 
-class Character
+using namespace std;
+
+class Monster;
+
+class Character : public Actor
 {
+private:
+	static Character* Instance; //싱글톤 패턴을 위한 정적 멤버 변수
+
+	//멤버 변수
+	string Name;
+	int Level;
+	int MaxHealth;
+	int Health;
+	int Power;
+	int Exp;
+	int Gold;
+
+	Character(); //생성자 (private으로 선언하여 외부에서 인스턴스 생성을 막음)
+
 public:
-    
+	static Character* GetInstance(); //싱글톤 인스턴스 반환
+
+	//멤버 함수
+	void TakeDamage(int damage);
+	void Heal(int amount);
+	void GainExp(int amount);
+	void LevelUp();
+	
+	//Getter
+	string GetStatus();
+	string GetName() const;
+	int GetLevel() { return Level; }
+	int GetMaxHealth() const;
+	int GetHealth() const;
+	int GetPower() const;
+
+	//Setter
+	void SetName(const string& Name);
+	void SetLevel(int Level);
+	void SetMaxHealth(int MaxHealth);
+	void SetHealth(int Health);
+	void SetPower(int Power);
+
+	void PlayerStatus();
+	void TakeDamage(int Damage);
+	virtual void Attack(Monster* monster);
+	virtual ~Character();
+
 };
