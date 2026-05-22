c8e2b5098366bc41f076e429e62b549749fb1c6e
huidong8809 <huidong8809@gmail.com>
2026-05-22 20:27:10 +0900

    feat : GameLog 헤더파일 작성
    GameLog 헤더파일 기초 틀 완성

--- dnf/GameSystem/GameLog.cpp
+++ dnf/GameSystem/GameLog.cpp
@@ -1,1 +1,37 @@
-﻿#include "GameLog.h"
+﻿#include <iostream>
+
+#include "GameLog.h"
+#include "../Character/Character.h"
+#include "../Monster/Monster.h"
+#include "../Item/Item.h"
+
+GameLog::GameLog()
+{
+    CharacterAttackCount = 0;
+    TotalDealtDamage = 0;
+    TotalReceivedDamage = 0;
+
+   
+    ClearBattleLog(); // 배틀 카운트 초기화
+}
+
+void GameLog::ClearBattleLog()
+{
+    CurrentBattleAttackCount = 0;
+    CurrentBattleDealtDamage = 0;
+    CurrentBattleReceiveCount = 0;
+    CurrentBattleReceiveDamage = 0;
+}
+
+void GameLog::LogCharacterSpawn(const Character& CharacterRef)
+{
+}
+
+// 몬스터 스폰 기록 및 카운트 누적
+void GameLog::LogMonsterSpawn(const Monster& MonsterRef)
+{
+    string name = MonsterRef.GetName();
+    MonsterSpawnCount[name]++; // 몬스터별 스폰 횟수 누적
+
+    cout << " [" << name << "] 등장 (누적 스폰: " << MonsterSpawnCount[name] << "회)" << endl;
+}
--- dnf/GameSystem/GameLog.h
+++ dnf/GameSystem/GameLog.h
@@ -1,7 +1,55 @@
-﻿#pragma once
+﻿#ifndef LOG_H
+#define LOG_H
+
+#include <string>
+#include <vector>
+#include <map> // 잡은 몬스터 기록을 위한 장부
+
+class Character; // 전방선언
+class Monster;
+class Item;
 
 class GameLog
 {
 public:
-    
+	GameLog(); // 생성자
+
+	void ClearBattleLog(); // 새로운 배틀이 시작되면 화변에 카운터 변수를 0으로 초기화
+
+	void LogCharacterSpawn(const Character& CharacterRef); // 플레이어 객채 받아오는 함수 (참조자)
+	void LogMonsterSpawn(const Monster& MonsterRef); // 몬스터 객채 받아오는 함수 (참조자)
+
+	void LogPlayerAttack(std::string MonsterName, int Damage); // 플레이어가 피해
+	void LogPlayerDamaged(std::string MonsterName, int Damage); // 플레이어가 피격
+	void LogItemUse(const Item& ItemRef); // 아이템 사용
+	void LogMonsterKill(std::string MonsterName); // 처치 몬스터 누적
+
+	void LogSceneChange(std::string Location); // 메뉴 이동 시 출력 
+	void PrintVisitedLog(); // 이동 경로 쌓아서 보여줌
+
+
+
+
+private:
+	
+	// 새로운 몬스터를 만나서 조우 할 때 마다 초기화 됨
+	int CurrentBattleAttackCount; //이번 배틀에서 공격 횟수
+	int CurrentBattleDealtDamage; //이번 배틀에서 내가 가한 누적 데미지
+	int CurrentBattleReceiveCount; //이번 배틀에서 내가 입은 누적 피해량
+	int CurrentBattleReceiveDamage; //이번 배틀에서 내가 입은 총 피해량
+
+	int CharacterAttackCount;                       //  총 공격 횟수
+	int TotalDealtDamage;                           //  누적 가한 데미지
+	int TotalReceivedDamage;                        //  누적 입은 피해량
+	std::map<std::string, int> MonsterKills;        //  잡은 몬스터 누적
+	std::map<std::string, int> ItemUseCount;        //  각 아이템 사용 횟수
+	std::map<std::string, int> MonsterSpawnCount;   //  몬스터 스폰 카운트
+
+	std::vector<std::string> VisitedLocations; // 플레이어 이동 경로 저장
+
+
+
+
 };
+
+#endif
