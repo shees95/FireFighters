0742fd283d054db7258ec4f55e48089d838bdfdc
jyj261 <blackjack0200@gmail.com>
2026-05-26 15:05:28 +0900

    feat : Util namespace 작성

--- dnf/Interface/Util.h
+++ dnf/Interface/Util.h
@@ -1,9 +1,16 @@
 // Util.h
 
 #pragma once
+#include <string>
 
-class Util
+namespace Util
 {
-public:
+    // 범위 내 정수 입력 받기
+    int SelectorInt(int min, int max);
 
-};
+    // 예/아니오 입력 받기
+    bool SelectorBool();
+
+    // 문자열 입력 받기 (유효성 검사 포함)
+    std::string SelectorString(std::string& prompt);
+}
