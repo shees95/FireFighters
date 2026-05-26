7aef1bc63c5112ce14109228b48ce0e3834e6830
chanchan0810 <imchanwoo5302@gmail.com>
2026-05-26 15:10:55 +0900

    fix/Item-Pont,include-string
    폰트변경 및 UTF-8적용
    헤더파일에 include<string>추가

---
+++ dnf/Item/Inventory.cpp
@@ -0,0 +1,67 @@
+#include"Inventory.h"
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void Inventory::AddItem(Item item)            //inventory.addItem(hpPotion);
+                                              //inventory.addItem(attackPotion);
+{                                             // 메인 함수에서 이걸로 추가가능
+    items.push_back(item);
+
+    cout << item.GetName()<< " 획득!" << endl;
+}
+
+bool Inventory::RemoveItem(Item item)
+{
+    for (int i = 0; i < items.size(); i++)
+    {
+        if (items[i].GetName() == item.GetName())
+        {
+            items.erase(items.begin() + i);
+            cout << item.GetName() << " 제거 완료!" << endl;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void Inventory::SortItemByPrice()
+{
+    sort(items.begin(), items.end(),
+        [](Item a, Item b)
+        {
+            return a.GetPrice() < b.GetPrice();
+        });
+
+    cout << "가격 오름차순 정렬 완료!"
+        << endl;
+}
+
+void Inventory::UseRandomItem(Actor* actor)
+{
+    {
+        if (items.empty())
+        {
+            cout << "사용할 아이템이 없습니다."
+                << endl;
+
+            return;
+        }
+
+        int randomIndex = rand() % items.size();
+
+        cout << items[randomIndex].GetName()
+            << " 랜덤 사용!"
+            << endl;
+
+        items[randomIndex].Use(actor);
+
+        cout << items[randomIndex].GetName() << "이 인벤토리에서 사용되었습니다." << endl;;
+       
+        items.erase(items.begin() + randomIndex);
+
+
+    }
+}
--- dnf/Item/Item.h
+++ dnf/Item/Item.h
@@ -1,7 +1,48 @@
 ﻿#pragma once
 
-class Item
+#include <string>
+#include "../Interface/Actor.h"
+
+
+//- [ ]  아이템 사용 조건 : 자율
+//-[]  아이템은 사용 순간 사라지고, 효과는 바로 캐릭터에게 반영
+//- []  아이템 : **체력 + 50(포션)또는 공격력 + 10 증가 * *
+//-[]  체력 + 50(포션) : 현재 체력을 50 회복
+//- []  공격력 + 10 : 해당 전투에만 효과 발휘
+//- []  플레이어 캐릭터 : 전투 중 아이템 랜덤 사용 가능
+
+
+enum class ItemType
 {
+    Equip,
+    Use,
+    Etc
+};
+
+
+
+class Item : public Actor
+{
+
 public:
+    Item(std::string Name, ItemType Type, int EffectType, int EffectValue, int Price);
+
     
+    void Use(Actor* actor);
+    
+    std::string GetName() { return Name; };
+    int GetPrice() { return Price; };
+
+
+private:
+    std::string Name;
+
+    ItemType Type;
+
+    int EffectType;
+
+    int EffectValue;
+
+    int Price;
+
 };
